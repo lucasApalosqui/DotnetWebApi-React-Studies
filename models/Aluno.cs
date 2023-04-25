b481a49e9b4234379a32864e5da4b1ce8bdcc240
LucasApalosqui <lucas.apalosqui@gmail.com>
2023-04-25 18:23:27 -0300

    @lucasApalosqui [ TASK: Add folder models and class Aluno ]

---
+++ models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosApi.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80, ErrorMessage = "The number of characters cannot exceed 80")]
+        [MinLength(3, ErrorMessage = "The number of characters cannot be less than 3")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(80, ErrorMessage = "The number of characters cannot exceed 80")]
+        [MinLength(5, ErrorMessage = "The number of characters cannot be less than 3")]
+        public string Email { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+    }
+}
